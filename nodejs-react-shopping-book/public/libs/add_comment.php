5c6ce04abf92a987c60d2481ae26183403643698
deymon <dimag2811@gmail.com>
2022-01-20 22:07:37 +0200

    site2

---
+++ nodejs-react-shopping-book/public/libs/add_comment.php
@@ -0,0 +1,7 @@
+<?php
+  $name = $_POST['name'];
+  $comment = $_POST['comment'];
+  $mysqli = new Mysqli('localhost', 'root', '', 'comments');
+  $mysqli->query("SET NAMES utf8");
+  $mysqli->query("INSERT INTO `comm`(`name`, `comment`) VALUES('$name', '$comment')");
+?>
